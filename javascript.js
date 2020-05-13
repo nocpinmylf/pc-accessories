0eeda6918fe49dd97e4049b6064b319c0c1ce545
nocpinmylf <lemduc@hotmail.com>
2020-05-13 20:16:57 +0700

    initial

---
+++ javascript.js
@@ -0,0 +1,7 @@
+const collapseBTN = document.getElementById('collaspe-btn');
+const headerNav = document.getElementsByClassName('nav')[0];
+
+// header
+collapseBTN.addEventListener('click', () => {
+  headerNav.classList.toggle('hide');
+});
