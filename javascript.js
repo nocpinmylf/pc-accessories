2f545286381334cd31ba6fba71ac092401ae3099
nocpinmylf <lemduc@hotmail.com>
2020-05-14 12:08:48 +0700

    update

--- javascript.js
+++ javascript.js
@@ -1,7 +1,19 @@
 const collapseBTN = document.getElementById('collaspe-btn');
-const headerNav = document.getElementsByClassName('nav')[0];
+const headerNavContent = document.getElementsByClassName('nav')[0];
+const headerNav = document.querySelector('.header-nav');
 
 // header
 collapseBTN.addEventListener('click', () => {
-  headerNav.classList.toggle('hide');
+  headerNavContent.classList.toggle('hide');
 });
+
+window.addEventListener('scroll', () => {
+  console.log(window.pageYOffset)
+  if(window.pageYOffset > 150) {
+    headerNav.classList.add('show');
+  }
+
+  else {
+    headerNav.classList.remove('show');
+  }
+})
